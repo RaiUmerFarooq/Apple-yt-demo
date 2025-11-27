cdd1d1faa9235f678c60b181dc321581578044be
Rai Umer <raifarooq7860786@gmail.com>
2025-11-27 22:12:01 +0500

    We made a Hero section just playing videos

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useRef } from 'react';
+
+const Hero = () => {
+    const videoRef = useRef(null);
+    useEffect(()=>{
+        if(videoRef.current){
+            videoRef.current.playbackRate = 3;
+        }
+    },[])
+  return (
+   <section id='hero'>
+    <div>
+        <h1>
+            MackBook Pro
+        </h1>
+        <img src="/title.png" alt="MacBook Pro Title" />
+    </div>
+    <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted playsInline />
+   </section>
+  )
+}
+
+export default Hero
