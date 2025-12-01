65a8dae824993b4816430efce98d65eeef25b138
Rai Umer <raifarooq7860786@gmail.com>
2025-12-01 22:03:13 +0500

    Converted 3D models to jsx using gltfjsx and using that in productviewer

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    // This fixes the "Blocked request" error
+    allowedHosts: [
+      'applepiper.deltainnovations.net'
+    ],
+    // This fixes the "Network" issue (replaces --host flag)
+    host: true
+  }
 })
