22a9f287ef5eea71b58c77b6eb8741d8a8202ff1
Rai Umer <raifarooq7860786@gmail.com>
2025-12-08 22:39:46 +0500

    Using macbook model to timline

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,41 @@
+import { Canvas } from "@react-three/fiber"
+import StudioLights from "./three/StudioLights"
+import { features } from "../constant"
+import clsx from 'clsx'
+import { Suspense, useRef } from "react"
+import { Html } from "@react-three/drei"
+import {useMediaQuery} from 'react-responsive'
+import MacbookModel from "./models/Macbook"
+const ModelScroll = () =>{
+  const groupRef = useRef(null);
+  const isMobile = useMediaQuery({query : '(max-width: 1024px)'})
+  return(
+    <group ref={groupRef}>
+      <Suspense fallback={<Html><h1 className="text-white text-3xl uppercase">loading...</h1> </Html>} >
+      <MacbookModel scale={isMobile?0.05:0.08} position={[0,-1,0]} />
+      </Suspense>
+    </group>
+  )
+}
+
 const Features = () => {
   return (
-    <div>Features</div>
+    <section id="features">
+      <h2>see it all in a new light.</h2>
+      <Canvas id="f-canvas" camera={{}}>
+        <StudioLights />
+        <ambientLight intensity={0.5} />
+        <ModelScroll />
+      </Canvas>
+      <div className=" absolute inset-0">
+        {features.map((feature, index) => (
+          <div key={index} className={clsx('box', `box${index + 1}`, feature.styles)}>
+            {feature.text}
+          </div>
+        ))}
+
+      </div>
+    </section>
   )
 }
 
